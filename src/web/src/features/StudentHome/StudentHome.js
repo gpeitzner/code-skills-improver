1bbfbef27ccb8c6bfc32cb83c12e8ba9fa49fa8c
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-10-29 10:12:43 -0600

    [add] tests component

--- src/web/src/features/StudentHome/StudentHome.js
+++ src/web/src/features/StudentHome/StudentHome.js
@@ -8,16 +8,52 @@ import {
 	CardMedia,
 	CardContent,
 	Typography,
+	TableContainer,
+	Paper,
+	Table,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableBody,
 } from "@mui/material";
 
 import problemImage from "../../assets/problem_image.jpg";
+import testsImage from "../../assets/tests_image.jpg";
+import { Check, Error } from "@mui/icons-material";
 
 function StudentHome() {
+	function createData(number, input, output, status) {
+		return { number, input, output, status };
+	}
+
+	const rows = [
+		createData(
+			"1",
+			`nums = [8, 7, 2, 5, 3, 1]; target = 10;`,
+			`Pair found (8, 2) or Pair found (7, 3)`,
+			false
+		),
+		createData(
+			"2",
+			`nums = [5, 2, 6, 8, 1, 9]; target = 12;`,
+			`Pair not found`,
+			true
+		),
+	];
+
 	return (
 		<div className="StudentHome">
 			<Box sx={{ flexGrow: 1, width: "100%", heigth: "90vh" }}>
 				<Grid container spacing={2}>
-					<Grid item xs={12} md={6} sx={{ height: "45vh" }}>
+					<Grid
+						item
+						xs={12}
+						sm={12}
+						md={12}
+						lg={6}
+						xl={6}
+						sx={{ height: "45vh" }}
+					>
 						<Card sx={{ height: "100%" }}>
 							<CardHeader title="Problem" />
 							<CardMedia component="img" height="100" image={problemImage} />
@@ -32,13 +68,77 @@ function StudentHome() {
 							</CardContent>
 						</Card>
 					</Grid>
-					<Grid item xs={12} md={6}>
+					<Grid
+						item
+						xs={12}
+						sm={12}
+						md={12}
+						lg={6}
+						xl={6}
+						sx={{ height: "45vh" }}
+					>
 						<p>Item 2</p>
 					</Grid>
-					<Grid item xs={12} md={6}>
-						<p>Item 3</p>
+					<Grid
+						item
+						xs={12}
+						sm={12}
+						md={12}
+						lg={6}
+						xl={6}
+						sx={{ height: "45vh" }}
+					>
+						<Card sx={{ height: "100%" }}>
+							<CardHeader title="Tests" />
+							<CardMedia component="img" height="100" image={testsImage} />
+							<CardContent>
+								<TableContainer component={Paper}>
+									<Table
+										sx={{ minWidth: 650 }}
+										size="small"
+										aria-label="a dense table"
+									>
+										<TableHead>
+											<TableRow>
+												<TableCell>#</TableCell>
+												<TableCell>Input</TableCell>
+												<TableCell>Output</TableCell>
+												<TableCell>Status</TableCell>
+											</TableRow>
+										</TableHead>
+										<TableBody>
+											{rows.map((row, i) => (
+												<TableRow
+													key={i}
+													sx={{
+														"&:last-child td, &:last-child th": { border: 0 },
+													}}
+												>
+													<TableCell component="th" scope="row">
+														{row.number}
+													</TableCell>
+													<TableCell>{row.input}</TableCell>
+													<TableCell>{row.output}</TableCell>
+													<TableCell>
+														{row.status ? <Check /> : <Error />}
+													</TableCell>
+												</TableRow>
+											))}
+										</TableBody>
+									</Table>
+								</TableContainer>
+							</CardContent>
+						</Card>
 					</Grid>
-					<Grid item xs={12} md={6}>
+					<Grid
+						item
+						xs={12}
+						sm={12}
+						md={12}
+						lg={6}
+						xl={6}
+						sx={{ height: "45vh" }}
+					>
 						<p>Item 4</p>
 					</Grid>
 				</Grid>
