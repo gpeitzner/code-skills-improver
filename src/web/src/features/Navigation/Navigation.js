2d17de55a6a93d13c1dfe6930f5cd5537d929169
Guillermo Peitzner <guillermopeitzner@gmail.com>
2024-04-22 22:04:07 -0600

    fix: navigation bar

--- src/web/src/features/Navigation/Navigation.js
+++ src/web/src/features/Navigation/Navigation.js
@@ -7,44 +7,51 @@ import { useCookies } from "react-cookie";
 import ExitToApp from "@mui/icons-material/ExitToApp";
 
 function Navigation() {
-	const [cookie, , removeCookie] = useCookies(["access"]);
-	const history = useHistory();
+  const [cookie, , removeCookie] = useCookies(["access"]);
+  const history = useHistory();
 
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}
-					>
-						<Code />
-					</IconButton>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-						Code Skills Improver
-					</Typography>
-					{cookie["access"] !== undefined && (
-						<IconButton
-							size="large"
-							aria-label="account of current user"
-							aria-controls="menu-appbar"
-							aria-haspopup="true"
-							onClick={() => {
-								removeCookie("access");
-								history.push("/");
-							}}
-							color="inherit"
-						>
-							<ExitToApp />
-						</IconButton>
-					)}
-				</Toolbar>
-			</AppBar>
-		</Box>
-	);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => history.push("/")}
+          >
+            <Code />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            onClick={() => history.push("/")}
+            style={{ cursor: "pointer" }}
+          >
+            Code Skills Improver
+          </Typography>
+          {cookie["access"] !== undefined && (
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={() => {
+                removeCookie("access");
+                history.push("/");
+              }}
+              color="inherit"
+            >
+              <ExitToApp />
+            </IconButton>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default Navigation;
